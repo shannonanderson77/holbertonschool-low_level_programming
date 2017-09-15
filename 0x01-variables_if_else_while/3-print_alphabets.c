057a3b28dd76376aae096ceaac30131f08ffcc41
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-15 15:51:58 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+* print the alphabet in lowercase
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+/* your code goes there */
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
