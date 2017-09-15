193733706de81c830ead6d9f090acb2e12196a60
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-15 01:39:33 +0000

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
- *
+ * generate random number and display whether it's positive, negative or zero
  * Return: Always 0 (Success)
  */
 int main(void)
