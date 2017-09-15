165d8d97ecf07d86fa04d3ce7ac26924b782b6a0
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-15 18:32:59 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+* print the alphabet in lowercase
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+/* your code goes there */
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
