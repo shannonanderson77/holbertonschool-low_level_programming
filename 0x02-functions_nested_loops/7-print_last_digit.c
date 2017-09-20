d44b91b67927273946aa1334939aaab6c8b2db93
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-20 05:54:51 +0000

    Jack Bauer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * print_last_digit - prints the last digit of a number
+  * @i: takes an integer value
+  * Return: value of last digit of integer
+  */
+int print_last_digit(int i)
+{
+	if (i = 0; i <= 9; i++)
+	_putchar(i + '0');
+	return (i % 10);
+	
+}
