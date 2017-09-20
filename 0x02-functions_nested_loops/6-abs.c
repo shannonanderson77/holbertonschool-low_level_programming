f11f3250428c182fb40bcdc834a774ccf824461e
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-20 02:41:03 +0000

    There is no such thing

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  * _abs - displays the absolute value of an integer
+  * @int: takes the value of any integer
+  * Return: absolute value
+  */
+int _abs(int r)
+{
+
+	if (r < 0)
+		return (-r);
+	else
+		return (r);
+}
