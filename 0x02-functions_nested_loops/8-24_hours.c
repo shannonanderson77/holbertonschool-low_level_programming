cf8d1d53e6eab17854e6559212361f7a8c943011
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-20 05:44:49 +0000

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * jack_bauer - prints every minute of the day of Jack Bauer
+  *
+  * Returns: void
+  */
+void jack_bauer(void) {
+	int  hour, min;
+
+	for (hour = 0; hour <= 23; hour++) {
+
+		for (min = 0; min <= 59; min++) {
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
