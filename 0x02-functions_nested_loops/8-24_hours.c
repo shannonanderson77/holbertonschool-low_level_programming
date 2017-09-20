e894971605c83aea6dd1fc1849959796feccd72f
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-20 05:51:49 +0000

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,12 +5,14 @@
   *
   * Returns: void
   */
-void jack_bauer(void) {
+void jack_bauer(void)
+{
 	int  hour, min;
 
-	for (hour = 0; hour <= 23; hour++) {
-
-		for (min = 0; min <= 59; min++) {
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
 			_putchar((hour / 10) + 48);
 			_putchar((hour % 10) + 48);
 			_putchar(':');
