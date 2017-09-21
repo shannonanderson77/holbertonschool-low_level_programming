3741ea79ab2f9b72c4af1c18f7188543fd1d64ca
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-21 09:58:06 +0000

    I believe

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  * print_most_numbers - print all numbers 0 thru 9
+  * except 2 and 4
+  *
+  * Return: list of numbers 0 thru 9 except 2 and 4
+  */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((i != '2') && (i != '4'))
+			_putchar(i);
+	}
+	_putchar('\n');
+}
