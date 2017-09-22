3266b190ec4236f68f291679f9ea861ece6c91a2
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-22 06:10:34 +0000

    FizzBuzz

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,28 @@ int main(void)
 {
 int i;
 
+if (i < 100)
+printf('\n');
+{
 for (i = 1; i <= 100; i++)
 {
 	if ((i % 3 == 0) && (i % 5 == 0))
 	{
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 	}
 	else if (i % 5 == 0)
 	{
-		printf("Buzz ");
+		printf("Buzz");
 	}
 	else if (i % 3 == 0)
 	{
-		printf("Fizz ");
+		printf("Fizz");
 	}
 	else
 	{
-		printf("%d ", i);
+		printf("%d", i);
 	}
 } printf("\n");
-	return (0);
+
+} return (0);
 }
