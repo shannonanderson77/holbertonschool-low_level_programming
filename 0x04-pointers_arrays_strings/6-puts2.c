dbce0cd4163a5bd52724461f2f694abc1fed4451
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-27 19:25:01 +0000

    Half the lies

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,13 @@
   */
 void puts2(char *str)
 {
+	int counter = 0;
+
 	while (*str != '\0')
 	{
-	if (*str % 2 == 0)
+	if (counter % 2 == 0)
 		_putchar(*str);
+		counter++;
 		str++;
 	}
 	_putchar('\n');
