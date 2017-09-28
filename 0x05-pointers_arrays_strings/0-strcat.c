829aab699f37b62c14a0a81415a8a35b561c660f
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-28 23:55:55 +0000

    strcat

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  * _strcat - a function that concatenates two strings
+  * @dest: takes the first string
+  * @src: takes the second string
+  * Return: p
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (i < '\0')
+	{
+	i++;
+	dest++;
+	} return (dest[i] + src);
+}
