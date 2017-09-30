b3896f1566f91a8070afa19d2a60a894576de2cc
shannonanderson77 <shannonanderson77@gmail.com>
2017-09-30 04:29:41 +0000

    strncat

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+  * _strncat - a function that concatenates two strings
+  * if the second string in longer than n bytes, no need
+  * for a null terminator and only concatenate n characters
+  * @dest: takes first string
+  * @src: takes second string
+  * @n: number of bytes to concatenate
+  * Return: concatenated string in a pointer
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int j;
+
+i = 0;
+j = 0;
+while (dest[i] != '\0')
+{
+	i++;
+}
+if (src[j] < src[n])
+{
+while (src[j] != '\0' && src[j] < n)
+{
+	dest[i] = src[j] + '\0';
+	i++;
+	j++;
+}
+} else
+{
+	dest[i] = src[j];
+	i++;
+	j++;
+}
+return (dest);
+}
