b1315cfef35dd0a089f11c8d94123589c24360b8
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-04 23:33:04 +0000

    Dreams feel real

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * _strlen_recursion - a function that returns the length
+  * of a string
+  * @s: takes a string
+  * Return: the length of the string
+  */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(s + 1));
+}
