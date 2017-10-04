66f1fbafb056ba18eb77d3f04b19b1e1cafd9711
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-04 20:23:22 +0000

    She locked away a secret

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * _puts_recursion - a function that prints a string
+  * followed by a new line
+  * @s: takes a string to be printed
+  * Return: nothing.
+  */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+if (s[i] == '\0')
+{
+	_putchar('\n');
+}
+_putchar(s[i]);
+_puts_recursion(s + 1);
+i++;
+return;
+}
