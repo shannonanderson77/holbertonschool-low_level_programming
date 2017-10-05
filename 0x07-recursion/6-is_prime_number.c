c780f166ec9e670fe79743b06a6a2627f5701a23
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-05 04:39:16 +0000

    Inception

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  * is_prime_number - a function that returns 1 if the input
+  * integer is a prime number
+  * @n: takes an integer
+  * Return: 1 if n is prime, otherwise 0
+  */
+int is_prime_number(int n)
+{
+
+
+}
