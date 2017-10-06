2215283239b0eac4709338d9a2c5c73819e9cfd4
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-06 15:49:12 +0000

    It ain't what they call you

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - a program that prints its name
+  * followed by a new line
+  * @argc: program name
+  * @argv: array that holds argc values
+  * Return: nothing
+  */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (argc);
+}
