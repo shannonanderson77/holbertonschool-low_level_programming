e694d7544d076eac1235a10a0992fd0a2598a498
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-06 17:07:56 +0000

    Inception

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
+  * checker - a function that checks if number is prime
+  * @x: takes an integer
+  * @y: takes an integer
+  * Return: an integer
+  */
+int checker(int x, int y)
+{
+if (y == 1)
+	return (1);
+if (y < 1)
+	return (1);
+if (x % y == 0)
+	return (0);
+else
+	return (checker(x, y - 1));
+}
+
+/**
   * is_prime_number - a function that returns 1 if the input
   * integer is a prime number
   * @n: takes an integer
@@ -8,6 +26,13 @@
   */
 int is_prime_number(int n)
 {
-
-
+if (n == 1)
+{
+return (1);
+}
+if (n < 1)
+{
+return (0);
+}
+return (checker(n, n - 1));
 }
