85faf4f2a260422c9de51a1ed039c1cfe6731c4b
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-06 21:18:03 +0000

    Silence is argument

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -7,10 +7,10 @@
   * of arguments passed into it followed by a new line
   * @argc: number of things passed to program
   * @argv: unused array that holds things passed to program
-  * Return: nothing
+  * Return: integer
   */
 int main(int argc, char __attribute__((unused))*argv[])
 {
 printf("%d\n", argc - 1);
-return (0);
+return (argc);
 }
