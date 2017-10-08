6a11943e66e64cbb6254b03ca8df09dec26072eb
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-08 23:09:03 +0000

    fix

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
   * @argv: array that holds argc values
   * Return: nothing
   */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused))argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
