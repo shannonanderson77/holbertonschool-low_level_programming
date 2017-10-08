d87bb4026ca10d4a1c3fa61a5f32337e76b2ec72
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-08 23:29:18 +0000

    fix

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -3,7 +3,8 @@
 #include <ctype.h>
 #include "holberton.h"
 
-/** main - a program that adds positive numbers
+/**
+  * main - a program that adds positive numbers
   * @argc: a count of arguments supplied to program
   * @argv: an array of pointers to the strings of argc
   * Return: an integer of Error if one of the numbers
@@ -12,23 +13,9 @@
 int main(int argc, char *argv[])
 {
 int i;
-/*int j;
-char ptr = *argv[];*/
 int sum = 0;
 
-/*for (j = 0; j < 255; j++)
-{
-	for (ptr = 0; ptr < 255; ptr++)
-	{
-if (ptr < 48 || ptr > 57)
-{
-printf("Error\n");
-} return (1);
-}*/
-
-
-
-	for (i = 1; i < argc; i++)
+for (i = 1; i < argc; i++)
 	{
 	sum = sum + atoi(argv[i]);
 	} printf("%d\n", sum);
