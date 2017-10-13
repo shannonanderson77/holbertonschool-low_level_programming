7c690580b914107083b3de83f347bcb82d21eaf0
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-13 01:03:34 +0000

    _calloc

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int *p;
+char *p;
 unsigned int i;
 
 if (nmemb == 0)
@@ -18,7 +18,7 @@ if (nmemb == 0)
 if (size == 0)
 	return (NULL);
 
-p = malloc(nmemb * size);
+p = malloc(nmemb * sizeof(int) * size);
 if (p == NULL)
 	return (NULL); /* if malloc fails, _calloc returns NULL */
 
