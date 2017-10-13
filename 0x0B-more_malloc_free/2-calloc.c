31d1a2bbbbf3b8f79bcc50a95fdc87fc4120392c
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-13 03:06:56 +0000

    _calloc

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -21,6 +21,9 @@ p = malloc(nmemb * size);
 if (p == NULL)
 	return (NULL); /* if malloc fails, _calloc returns NULL */
 for (i = 0; i <= nmemb; i++)
+{
 	p[i] = 0; /* set memory to zero */
+}
 return (p);
+free(p);
 }
