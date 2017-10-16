6cf93e8ff905553c61ffbf728a5e7244ff7144b8
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-16 20:58:19 +0000

    File name

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+  * main - a program that prints the name of the file
+  * it was compiled from
+  *
+  * Return: 0
+  */
+int main(void)
+{
+printf("%s\n", __FILE__);
+	return (0);
+}
