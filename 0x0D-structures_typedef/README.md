58ef5a7e6da6298a31d7ec915b76f1fdd83e6d9e
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-16 23:43:25 +0000

    Django

---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,8 @@
+<h1>Readme</h1>
+<h2>C - Structures, typedef</h2>
+<ul>
+<li>Structures</li>
+<li>Typedef and structures</li>
+<li>Documentation: structures</li>
+</ul>
+<a href="https://www.holbertonschool.com/"><img src="https://www.holbertonschool.com/assets/holberton-logo-1cc451260ca3cd297def53f2250a9794810667c7ca7b5fa5879a569a457bf16f.png" /></a>
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - declaring a new type called dog
+  * @name: dog's name
+  * @owner: dog's owner
+  * @age: dog's age
+  *
+  * Description: using typedef and struct, a new variable
+  * dog is being declared
+  */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog;
+int _putchar(char c);
+#endif
