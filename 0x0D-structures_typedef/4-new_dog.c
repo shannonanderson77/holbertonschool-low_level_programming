ea65452c7c608aac6d6d935a5d55ea761207c485
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-17 06:57:47 +0000

    A new dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -16,8 +16,8 @@ char *p_name; /* a pointer to copy of name for new dog */
 char *p_owner; /* a pointer to copy of owner for new dog */
 int i;
 int counter; /* holds name length and owner length*/
-int name_length;
-int owner_length;
+int name_length = 0;
+int owner_length = 0;
 
 counter = 0;
 while (name[counter] != '\0')
@@ -36,11 +36,20 @@ while (owner[counter] != '\0')
 } /* determine length of copied owner */
 p_owner = malloc(owner_length *sizeof(char));
 if (p_owner == NULL)
+{
+	free(p_name);
 	return (NULL);
+}
 for (i = 0; name[i] != '\0'; i++)
-	p_name[i] = name[i]; /* make a copy of name */
+{
+	p_name[i] = name[i];
+}
+	p_name[i] = '\0'; /* make a copy of name */
 for (i = 0; owner[i] != '\0'; i++)
-	p_owner[i] = owner[i]; /* make a copy of owner */
+{
+	p_owner[i] = owner[i];
+}
+	p_owner[i] = '\0'; /* make a copy of owner */
 p_new_dog = malloc(sizeof(dog_t)); /* allocate memory for new dog */
 if (p_new_dog != NULL)
 {
@@ -50,6 +59,8 @@ p_new_dog->age = age;
 }
 else
 {
+free(p_name);
+free(p_owner);
 return (NULL);
 } return (p_new_dog);
 }
