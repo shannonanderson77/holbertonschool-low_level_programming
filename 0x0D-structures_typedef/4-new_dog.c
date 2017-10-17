c616cd4e80fc939d60d56389b4a4da3627b6ed13
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-17 07:29:07 +0000

    A new dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -15,40 +15,27 @@ dog_t *p_new_dog; /* a pointer to new dog of type dog_t*/
 char *p_name; /* a pointer to copy of name for new dog */
 char *p_owner; /* a pointer to copy of owner for new dog */
 int i;
-int counter; /* holds name length and owner length*/
 int name_length = 0;
 int owner_length = 0;
 
-counter = 0;
-while (name[counter] != '\0')
-{
-	name_length++;
-	counter++;
-} /* determines length of copied name string */
-p_name = malloc(name_length *sizeof(char));
+for (i = 0; name[i] != '\0'; i++)
+	name_length++; /* determines length of copied name string */
+p_name = malloc(name_length * sizeof(char) + 1);
 if (p_name == NULL)
 	return (NULL);
-counter = 0;
-while (owner[counter] != '\0')
-{
-	owner_length++;
-	counter++;
-} /* determine length of copied owner */
-p_owner = malloc(owner_length *sizeof(char));
+for (i = 0; owner[i] != '\0'; i++)
+	owner_length++; /* determine length of copied owner */
+p_owner = malloc(owner_length * sizeof(char) + 1);
 if (p_owner == NULL)
 {
 	free(p_name);
 	return (NULL);
 }
 for (i = 0; name[i] != '\0'; i++)
-{
 	p_name[i] = name[i];
-}
 	p_name[i] = '\0'; /* make a copy of name */
 for (i = 0; owner[i] != '\0'; i++)
-{
 	p_owner[i] = owner[i];
-}
 	p_owner[i] = '\0'; /* make a copy of owner */
 p_new_dog = malloc(sizeof(dog_t)); /* allocate memory for new dog */
 if (p_new_dog != NULL)
