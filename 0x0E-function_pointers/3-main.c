f65ac64f7ac5de3df6fdd14050f9bcb7b941f6e8
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-18 11:41:51 +0000

    A goal - setup

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+  * main - calculations main function
+  *
+  * Return: void
+  */
+int main(void)
+{
+
+}
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+  * op_add - a function that adds two integers
+  * @a: takes first integer
+  * @b: takes second integer
+  * Return: an integer
+  */
+int op_add(int a, int b)
+{
+
+}
+
+/**
+  * op_sub - a function that subtracts two integers
+  * @a: takes first integer
+  * @b: takes second integer
+  * Return: an integer
+  */
+int op_sub(int a, int b)
+{
+
+}
+
+/**
+  * op_mul - a function that multiplies two integers
+  * @a: takes first integer
+  * @b: takes second integer
+  * Return: an integer
+  */
+int op_mul(int a, int b)
+{
+
+}
+
+/**
+  * op_div - a function that divides two integers
+  * @a: takes first integer
+  * @b: takes second integer
+  * Return: an integer
+  */
+int op_div(int a, int b)
+{
+
+}
+
+/**
+  * op_mod - a function that gives the remainder of two integers
+  * @a: takes first integer
+  * @b: takes second integer
+  * Return: an integer
+  */
+int op_mod(int a, int b)
+{
+
+}
