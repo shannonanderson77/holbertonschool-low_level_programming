bc026628d46a57c780ef6d066bbbae5c749a6904
shannonanderson77 <shannonanderson77@gmail.com>
2017-10-22 18:25:51 +0000

    To be is a to be

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -18,27 +18,28 @@ int i;
 va_start(a_list, format);
 
 i = 0;
-while (format[i] != '\0')
+while (format[i] != '\0' && format != NULL)
 {
-switch(format[i]) {
-	case 'c':
-		printf("%c", (char) va_arg(a_list, int));
-		break;
-	case 's':
-		s = va_arg(a_list, char *);
-		if (s == NULL)
-			s = "(nil)";
-		printf("%s", s);
-		break;
-	
-	case 'f':
-		printf("%f", va_arg(a_list, double));
-		break;
-	case 'i':
-		printf("%d", va_arg(a_list, int));
-		break;
-	} 
-if ((format[i] == 'c' || format[i] == 's' || format[i] == 'f' || format[i] == 'i') && (format[i + 1] != '\0'))
+switch (format[i])
+{
+case 'c':
+	printf("%c", (char) va_arg(a_list, int));
+	break;
+case 's':
+	s = va_arg(a_list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+	break;
+case 'f':
+	printf("%f", va_arg(a_list, double));
+	break;
+case 'i':
+	printf("%d", va_arg(a_list, int));
+	break;
+}
+if ((format[i] == 'c' || format[i] == 's' ||
+	format[i] == 'f' || format[i] == 'i') && (format[i + 1] != '\0'))
 	printf(", ");
 i++;
 } printf("\n");
