8914bff746fd3aa88eab040f4c0f67f18e200696
shannonanderson77 <shannonanderson77@gmail.com>
2017-11-01 17:27:36 +0000

    fix

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,25 +18,32 @@ new_node = (listint_t *)malloc(sizeof(listint_t)); /*memory for new node*/
 if (new_node == NULL)
 	return (NULL);
 new_node->n = n; /*write data into new node*/
-if ((*head) == NULL) /*if the list is empty*/
-{
-*head = new_node;/*point head to new node*/
-new_node->next = NULL; /*point new node to NULL*/
-return (new_node);
-}
+if ((*head) == NULL && idx == 0) /*if the list is empty and index is 0*/
+	{
+	*head = new_node; /*point head to new node*/
+	new_node->next = NULL; /*point new node to NULL*/
+	return (new_node);
+	}
+if ((*head) == NULL)
+	return (NULL);
+if (idx == 0)
+	{
+	new_node->next = *head;
+	*head = new_node;
+	}
 temp_node = *head; /*hold head*/
-for (count = 0; count < idx && temp_node != NULL; count++)
+for (count = 0; count < idx - 1 && temp_node != NULL; count++)
 /*start counting till index or till temp node hits end of list*/
-{
-temp_node = temp_node->next; /*traverse the list*/
-}
+	{
+	temp_node = temp_node->next; /*traverse the list*/
+	}
 if (temp_node != NULL)
-{
+	{
 new_node->next = temp_node->next; /*done looping, make head point to new_node*/
-temp_node->next = new_node; /*make new node point to temp_node next*/
-return (new_node);
-}
+	temp_node->next = new_node; /*make new node point to temp_node next*/
+	return (new_node);
+	}
 else
-free(new_node);
+	free(new_node);
 return (NULL);
 }
