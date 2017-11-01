9e09af3769dd76996a8c60823b0d5a13857e29d0
shannonanderson77 <shannonanderson77@gmail.com>
2017-11-01 16:38:35 +0000

    Fix Get node at index

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int count;
 
-for (count = 0; count < index; count++)
+if (head == NULL)
+	return (head);
+for (count = 0; count < index && head != NULL; count++)
 	{
 	head = head->next;
 	}
-
-return (head);
+	return (head);
 }
