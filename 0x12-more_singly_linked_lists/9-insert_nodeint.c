4e0950d7fd4c17ea6c698090a3ae18bb88b5ba09
shannonanderson77 <shannonanderson77@gmail.com>
2017-11-01 17:11:44 +0000

    Fix Insert node

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+  * insert_nodeint_at_index - a function that inserts a new node
+  * at a given position
+  * @head: pointer to start of list
+  * @idx: index of the list where the new node should be added
+  * @n: data to write into new node
+  * Return: the address of the new node
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *temp_node; /*to hold head while inserting*/
+listint_t *new_node; /*new node to insert into list*/
+unsigned int count; /*to keep track of index a list is traversed*/
+
+new_node = (listint_t *)malloc(sizeof(listint_t)); /*memory for new node*/
+if (new_node == NULL)
+	return (NULL);
+new_node->n = n; /*write data into new node*/
+if ((*head) == NULL) /*if the list is empty*/
+{
+*head = new_node;/*point head to new node*/
+new_node->next = NULL; /*point new node to NULL*/
+return (new_node);
+}
+temp_node = *head; /*hold head*/
+for (count = 0; count < idx && temp_node != NULL; count++)
+/*start counting till index or till temp node hits end of list*/
+{
+temp_node = temp_node->next; /*traverse the list*/
+}
+if (temp_node != NULL)
+{
+new_node->next = temp_node->next; /*done looping, make head point to new_node*/
+temp_node->next = new_node; /*make new node point to temp_node next*/
+return (new_node);
+}
+else
+free(new_node);
+return (NULL);
+}
