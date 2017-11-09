f8c9e0946c31e739a7250b2da0d9b8a07be51553
shannonanderson77 <shannonanderson77@gmail.com>
2017-11-09 04:26:39 +0000

    Fix style

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 
-/** read_textfile - a function that reads a text file and prints it to the
-  * POSIX standard output
-  * @filename: file to be read
-  * @letters: number of letters function should read and print
-  * Return: number of letters function was able to read and print
-  */
+/**
+ * read_textfile - a function that reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: file to be read
+ * @letters: number of letters function should read and print
+ * Return: number of letters function was able to read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
@@ -24,5 +25,5 @@ if (read_ed == -1)
 if (write(STDOUT_FILENO, buffer, read_ed) == -1)
 	return (0);
 free(buffer);
-return(read_ed);
+return (read_ed);
 }
