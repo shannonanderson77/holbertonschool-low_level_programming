bf6450dff9647e3f6a2e93cfedc1745cbe2d8710
shannonanderson77 <shannonanderson77@gmail.com>
2017-12-17 01:21:06 +0000

    edge case fix

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,14 +15,16 @@ dlistint_t *temp_node = NULL;
 
 temp_node = *head;
 new_node = malloc(sizeof(dlistint_t));
+new_node->next = NULL;
 new_node->n = n;
+if (head == NULL)
+	return (NULL);
 if (new_node == NULL)
 {
 	return (NULL);
 }
 if (temp_node == NULL)
 {
-	new_node->next = NULL;
 	new_node->prev = NULL;
 	*head = new_node;
 	return (new_node);
@@ -31,7 +33,6 @@ while (temp_node->next != NULL)
 {
 	temp_node = temp_node->next;
 }
-new_node->next = NULL;
 new_node->prev = temp_node;
 temp_node->next = new_node;
 return (new_node);
