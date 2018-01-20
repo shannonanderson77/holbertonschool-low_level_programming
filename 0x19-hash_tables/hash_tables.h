f5e4c991c764cf0ce8e08f61269d853e647e7c2e
shannonanderson77 <shannonanderson77@gmail.com>
2018-01-20 07:01:07 +0000

    add header file

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -1,5 +1,8 @@
-# ifndef HASH_TABLES
-# define HASH_TABLES
+# ifndef HASH_TABLES_H
+# define HASH_TABLES_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
