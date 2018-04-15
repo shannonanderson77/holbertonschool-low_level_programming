de94889a5ee7056866fdfefbd785acaa707e5ed0
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-15 22:36:39 +0000

    add check for if index is 0, if index greater than list length

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -3,6 +3,26 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - a function that returns the number of elements
+ * in a linked dlistint_t list
+ * @h: pointer to the head of a doubly linked list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
+
+/**
  * get_dnodeint_at_index - a function that returns
  * the nth node of a dlistint_t linked list
  * @head: pointer to the start of a doubly linked list
@@ -11,14 +31,21 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int counter = 0;
+	dlistint_t *temp_node = NULL;
+	unsigned int length = 0;
 
+	length = dlistint_len(head);
 	if (head == NULL)
 		return (NULL);
-	while (head != NULL && counter < index)
+	if (index  >= length)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	temp_node = head;
+	while (temp_node != NULL && index > 0)
 	{
-		head = head->next;
-		counter++;
+		temp_node = temp_node->next;
+		index--;
 	}
-	return (head);
+	return (temp_node);
 }
