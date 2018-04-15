63b9ca8241e5f4098657a39cbce5196ca9039be6
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-15 04:08:06 +0000

    fix edge case of empty list

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -15,9 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	new_node->n = n;
+	new_node->prev = NULL;
 	if (new_node == NULL)
 		return (NULL);
-	new_node->prev = NULL;
+	if ((*head) != NULL)
+		(*head)->prev = new_node;
 	new_node->next = (*head);
 	(*head) = new_node;
 	return (new_node);
