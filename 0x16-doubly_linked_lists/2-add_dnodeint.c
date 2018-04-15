c7f35f07861bee640e9b023bcfe1578b7e5ca20e
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-15 22:49:24 +0000

    remove check for head not NULL

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->prev = NULL;
 	if (new_node == NULL)
 		return (NULL);
-	if (head == NULL || (*head) != NULL)
+	if (head == NULL)
 		(*head)->prev = new_node;
 	new_node->next = (*head);
 	(*head) = new_node;
