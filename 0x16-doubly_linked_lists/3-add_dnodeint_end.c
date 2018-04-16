4950df6c3f7a1846363984847891b0ce8d546fc9
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-16 00:24:48 +0000

    change syntax for best practice

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,14 +11,16 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL;
-	dlistint_t *temp_node = NULL;
+	dlistint_t *new_node;
+	dlistint_t *temp_node;
 
+	if (head == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
-	new_node->n = n;
-	new_node->next = NULL;
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
 	if ((*head) == NULL)
 	{
 		(*head) = new_node;
