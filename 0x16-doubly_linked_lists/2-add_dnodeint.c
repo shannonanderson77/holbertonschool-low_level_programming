6e83e2acab219d8b621c528f92786d7e8ad4f434
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-16 00:20:00 +0000

    add best practice suggestions

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -11,15 +11,15 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL;
+	dlistint_t *new_node;
 
 	if (head == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
-	new_node->n = n;
-	new_node->prev = NULL;
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
 	if ((*head) != NULL)
 	{
 		(*head)->prev = new_node;
