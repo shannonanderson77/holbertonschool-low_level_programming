845c296410ef40fc8f21bc56691497a89e31c0af
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-11 17:54:20 +0000

    change malloc arguments

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -13,12 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
-	if (size < 1)
-		return (NULL);
-	new_table = malloc(sizeof(hash_node_t));
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
-	new_table->array = malloc(sizeof(hash_table_t *) * size);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 		return (NULL);
 	new_table->size = size;
