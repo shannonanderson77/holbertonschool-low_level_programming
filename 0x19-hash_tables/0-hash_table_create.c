524cbcb91aced6534df867f398bcdd8cc4954d90
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-11 17:50:00 +0000

    add check for correct size value

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -13,15 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
+	if (size < 1)
+		return (NULL);
 	new_table = malloc(sizeof(hash_node_t));
 	if (new_table == NULL)
 		return (NULL);
 	new_table->array = malloc(sizeof(hash_table_t *) * size);
 	if (new_table->array == NULL)
 		return (NULL);
+	new_table->size = size;
 	for (i = 0; i < size; i++)
 	{
-		new_table->size = size;
 		new_table->array[i] = NULL;
 	}
 	return (new_table);
