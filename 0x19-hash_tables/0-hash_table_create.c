b2aa4ef77f641eec98567af7fed1ef3d692645c3
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-11 17:31:03 +0000

    fix betty errors

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,14 +15,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_node_t));
 	if (new_table == NULL)
-		return NULL;
+		return (NULL);
 	new_table->array = malloc(sizeof(hash_table_t *) * size);
 	if (new_table->array == NULL)
-		return NULL;
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		new_table->size = size;
 		new_table->array[i] = NULL;
-	}	
+	}
 	return (new_table);
 }
